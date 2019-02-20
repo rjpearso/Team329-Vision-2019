ba9561105eda9d96c0152343ad6dc854b0c97446
rybradlax <34609432+rybradlax@users.noreply.github.com>
2019-02-20 18:47:58 -0500

    Add files via upload
    Opencv quick install code

---
+++ cv2Install.py
@@ -0,0 +1,13 @@
+# Import the Necessary Packages
+import os
+
+os.system("sudo apt-get install python-tk")
+os.system("sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev")
+os.system("sudo apt-get install libavcodec-dev libavformat-dev libswscal-dev libv4l-dev")
+os.system("sudo apt-get install libxvidcore-dev libx264-dev")
+os.system("sudo apt-get install qt4-dev-tools")
+os.system("sudo pip3 install opencv-python")
+
+
+
+
