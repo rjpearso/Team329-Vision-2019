4d688edaf53e0d467b5763518bf5e9e370756ab3
rybradlax <34609432+rybradlax@users.noreply.github.com>
2019-04-06 15:24:16 -0400

    Update cv2Install.py

--- cv2Install.py
+++ cv2Install.py
@@ -3,10 +3,11 @@ import os
 
 os.system("sudo apt-get install python-tk")
 os.system("sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev")
-os.system("sudo apt-get install libavcodec-dev libavformat-dev libswscal-dev libv4l-dev")
+os.system("sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev")
+os.system("sudo apt-get install libcblas-dev libatlas-dev ")
 os.system("sudo apt-get install libxvidcore-dev libx264-dev")
 os.system("sudo apt-get install qt4-dev-tools")
-os.system("sudo pip3 install opencv-python")
+os.system("sudo pip3 install opencv-contrib-python")
 
 
 
